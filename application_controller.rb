c0443b51176fa4148c2df4f461a9e758f470578d
Yu Hang Chen <yuhangc9321@hstat.org>
2019-02-27 11:33:54 +0000

    spotify playlist working on backend

--- application_controller.rb
+++ application_controller.rb
@@ -1,5 +1,8 @@
 require 'dotenv/load'
 require 'bundler'
+require 'lyricfy'
+require 'pp'
+require 'rspotify'
 Bundler.require
 
 require_relative 'models/model.rb'
@@ -14,11 +17,15 @@ class ApplicationController < Sinatra::Base
     # puts params
     @user_name = params[:name]
     @user_playlist_id = params[:playlist_id]
+    @user_playlist = Playlist.new(@user_name,@user_playlist_id)
+    @user_playlist.get_info
+    @user_playlist.get_all_lyrics
+    pp @user_playlist
     
-    @user_title = params[:title]
-    @user_artist = params[:artist]
-    @user_song = Song.new(@user_title,@user_artist,"http://")
-    @user_song.get_lyrics
+    # @user_title = params[:title]
+    # @user_artist = params[:artist]
+    # @user_song = Song.new(@user_title,@user_artist,"http://")
+    # @user_song.get_lyrics
     
     erb :result
   end
--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,9 @@
 require 'lyricfy'
 require 'rspotify'
+require 'pp'
+
+RSpotify.authenticate("dc6d67e381b840d5972fa0c2ce423f70", "a48ab1d312204495a468a2601542ec02")
+
 
 # turn spotify user/playlist_id into playlist
 
@@ -44,3 +48,40 @@ end
 # dc6d67e381b840d5972fa0c2ce423f70
 # spotify client secret
 # a48ab1d312204495a468a2601542ec02
+
+# playlist = RSpotify::Playlist.find('guilhermesad', '1Xi8mgiuHHPLQYOw2Q16xv')
+# pp playlist.tracks.first
+# puts playlist.tracks.first.name
+# puts playlist.tracks.first.artists.first.name
+# puts playlist.tracks.first.album.images.first["url"]
+
+class Playlist
+    
+    attr_reader :user, :playlist_id, :name, :songs
+    
+    def initialize(user,playlist_id)
+        @user = user
+        @playlist_id = playlist_id
+        @songs = [] # instances of the Song class
+    end
+    
+    def get_info
+        playlist = RSpotify::Playlist.find(@user,@playlist_id)
+        @name = playlist.name
+        
+        playlist.tracks.each do |track|
+            @songs << Song.new(track.name,track.artists.first.name,track.album.images.first["url"])
+        end
+    end
+    
+    def get_all_lyrics
+       @songs.each do |song|
+           song.get_lyrics
+       end
+    end
+end
+
+# girl3 = Playlist.new("1114072956","7zGjKWLsPf7yyOm7d5hGR9")
+# girl3.get_info
+# puts girl3.name
+    
